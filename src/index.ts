848cf49dce91e36893aa8168acf98a4e11678740
zubayrlatief <zubayrlatie007@gmail.com>
2025-01-23 13:27:53 +0000

    register wrking

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-
-import dotenv from 'dotenv';
 import express from 'express';
 import authRoutes from './routes/authRoutes';
 
-
 const app = express();
-;
 
 app.use(express.json());
+
+// Routes
 app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-dotenv.config();
